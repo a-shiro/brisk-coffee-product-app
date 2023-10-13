e3396fa4399f3c119959745e2dab9d3ff48d5bab
a-shiro <alekmitov20@gmail.com>
2023-10-13 12:24:55 +0300

    Refactor: Move card display code to Cards component

--- src/pages/home/components/showcase/Showcase.jsx
+++ src/pages/home/components/showcase/Showcase.jsx
@@ -1,41 +1,15 @@
-// Hooks
-import { useEffect, useState } from "react";
 // Components
 import { Link } from "react-router-dom";
-import Card from "./components/Card";
+import Cards from "./components/cards/Cards";
 // CSS
 import styles from "./Showcase.module.css";
-// Data
-import { fetchDocs } from "../../../../services/queries";
 
 function Showcase() {
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchDocs("coffeeProducts");
-
-      setProducts(data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <section className={styles.section}>
       <h2 className={styles.title}>Triple Can Treat</h2>
 
-      <div className={styles.cards}>
-        {products?.map((product) => {
-          return (
-            <Card
-              flavour={product.flavour}
-              showcaseImage={product.showcaseImageUrl}
-              key={product.id}
-            />
-          );
-        })}
-      </div>
+      <Cards />
 
       <Link className={styles.orderBtn} to="/order-online">
         Order Online
