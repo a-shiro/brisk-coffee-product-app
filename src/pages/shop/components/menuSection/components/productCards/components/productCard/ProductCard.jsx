be3a4418b614c116c99a53f4df4dd50283876fab
a-shiro <alekmitov20@gmail.com>
2023-10-17 12:15:48 +0300

    Add: Open order menu modal functionality

--- src/pages/shop/components/menuSection/components/productCards/components/productCard/ProductCard.jsx
+++ src/pages/shop/components/menuSection/components/productCards/components/productCard/ProductCard.jsx
@@ -1,17 +1,33 @@
+// Hooks
+import { useContext } from "react";
 // CSS
 import styles from "./ProductCard.module.css";
+// Context
+import {
+  OrderModalContext,
+  ProductContext,
+} from "../../../../../../context/context";
 
-function ProductCard({ brand, flavour, price, storeImage }) {
+function ProductCard({ product }) {
+  const setModalVisible = useContext(OrderModalContext);
+  const setActiveProduct = useContext(ProductContext);
+
+  const openModal = () => {
+    setActiveProduct(product);
+    setModalVisible(true);
+    return;
+  };
+
   return (
-    <div className={styles.card}>
+    <div onClick={openModal} className={styles.card}>
       <div className={styles.cardInfo}>
-        <h4>{brand}</h4>
-        <p>{flavour}</p>
-        <span>${price.toFixed(2)}</span>
+        <h4>{product.brand}</h4>
+        <p>{product.flavour}</p>
+        <span>${product.price.toFixed(2)}</span>
       </div>
 
       <div className={styles.cardImageContainer}>
-        <img src={storeImage} alt="product" />
+        <img src={product.storeImageUrl} alt="product" />
       </div>
     </div>
   );
