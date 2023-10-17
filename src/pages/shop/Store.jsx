eee5289c6ba8c59b74c78a2d22e3eb1241d12c54
a-shiro <alekmitov20@gmail.com>
2023-10-17 12:21:22 +0300

    Add: Context and context providers that link to OrderModal functionality

--- src/pages/shop/Store.jsx
+++ src/pages/shop/Store.jsx
@@ -1,10 +1,18 @@
+// Hooks
+import { useState } from "react";
 // Components
 import MenuSection from "./components/menuSection/MenuSection";
 import Nav from "./components/nav/Nav";
+import OrderModal from "./components/orderModal/OrderModal";
 // CSS
 import styles from "./Store.module.css";
+// Context
+import { OrderModalContext, ProductContext } from "./context/context";
 
 function Store() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [activeProduct, setActiveProduct] = useState();
+
   return (
     <>
       <header className={styles.header}>
@@ -14,18 +22,28 @@ function Store() {
       <section className={styles.menu}>
         <Nav />
 
-        <MenuSection
-          sectionTitle="Coffee"
-          description="Experience Brisk Coffee's signature heartwarming drinks."
-          collectionName="coffeeProducts"
-        />
-
-        <MenuSection
-          sectionTitle="Other"
-          description="Other products by Brisk Coffee or partner companies"
-          collectionName="otherProducts"
-        />
+        <OrderModalContext.Provider value={setModalVisible}>
+          <ProductContext.Provider value={setActiveProduct}>
+            <MenuSection
+              sectionTitle="Coffee"
+              description="Experience Brisk Coffee's signature heartwarming drinks."
+              collectionName="coffeeProducts"
+            />
+            <MenuSection
+              sectionTitle="Other"
+              description="Other products by Brisk Coffee or partner companies"
+              collectionName="otherProducts"
+            />
+          </ProductContext.Provider>
+        </OrderModalContext.Provider>
       </section>
+
+      {modalVisible && (
+        <OrderModal
+          activeProduct={activeProduct}
+          setModalVisible={setModalVisible}
+        />
+      )}
     </>
   );
 }
---
+++ src/pages/shop/context/context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const OrderModalContext = createContext();
+
+export const ProductContext = createContext();
