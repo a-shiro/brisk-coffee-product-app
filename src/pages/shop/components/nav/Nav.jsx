73d9f185912765a3380cd1a7b565f109b4b19ef9
a-shiro <alekmitov20@gmail.com>
2023-10-18 14:20:33 +0300

    Add: Cart component

--- src/pages/shop/components/nav/Nav.jsx
+++ src/pages/shop/components/nav/Nav.jsx
@@ -1,15 +1,9 @@
-// Hooks
-import { useContext } from "react";
+// Components
+import Cart from "./components/Cart";
 // CSS
 import styles from "./Nav.module.css";
-// Resources
-import cartIcon from "../../../../resources/icons/shopping-cart.png";
-// Context
-import { CartContext } from "../../context/context";
 
 function Nav() {
-  const { cart } = useContext(CartContext);
-
   return (
     <nav className={styles.nav}>
       <ul className={styles.optionsList}>
@@ -17,10 +11,7 @@ function Nav() {
         <li>Other</li>
       </ul>
 
-      <div className={styles.cart}>
-        <img src={cartIcon} alt="cart" />
-        <span>{cart.quantity}</span>
-      </div>
+      <Cart />
     </nav>
   );
 }
