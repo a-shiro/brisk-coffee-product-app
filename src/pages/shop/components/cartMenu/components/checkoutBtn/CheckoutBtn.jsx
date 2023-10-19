d422865859eee361357ffca49798f3055e03f908
a-shiro <alekmitov20@gmail.com>
2023-10-19 16:42:13 +0300

    Add: CheckoutBtn component

---
+++ src/pages/shop/components/cartMenu/components/checkoutBtn/CheckoutBtn.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./CheckoutBtn.module.css";
+
+function CheckoutBtn() {
+  return (
+    <div className={styles.checkoutContainer}>
+      <button>Continue to Checkout</button>
+    </div>
+  );
+}
+
+export default CheckoutBtn;
