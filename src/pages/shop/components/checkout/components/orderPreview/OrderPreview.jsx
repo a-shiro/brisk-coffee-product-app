c2f66a86d3e524490e8fc7f982de615f02507742
a-shiro <alekmitov20@gmail.com>
2023-10-20 11:48:59 +0300

    Add: OrderPreview component

---
+++ src/pages/shop/components/checkout/components/orderPreview/OrderPreview.jsx
@@ -0,0 +1,15 @@
+function OrderPreview() {
+  return (
+    <div>
+      <header>
+        <h3>My Order (1)</h3>
+      </header>
+
+      {/* 
+      <ProductList />
+      <CheckoutBtn /> */}
+    </div>
+  );
+}
+
+export default OrderPreview;
