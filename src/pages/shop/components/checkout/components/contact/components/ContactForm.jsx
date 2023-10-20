0e401c4b31b2b557dc15cf672a7b3c52a7c13d2b
a-shiro <alekmitov20@gmail.com>
2023-10-20 12:13:19 +0300

    Add: ContactForm component

---
+++ src/pages/shop/components/checkout/components/contact/components/ContactForm.jsx
@@ -0,0 +1,58 @@
+// CSS
+import styles from "./ContactForm.module.css";
+
+function ContactForm({ fieldValues, setFieldValues, setComponentActive }) {
+  const changeHandler = (e) => {
+    setFieldValues((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setComponentActive(false);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={styles.contactForm}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        type="text"
+        value={fieldValues.firstName}
+        onChange={changeHandler}
+        required
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        type="text"
+        value={fieldValues.lastName}
+        onChange={changeHandler}
+        required
+      />
+      <label htmlFor="phone">Phone</label>
+      <input
+        id="phone"
+        type="number"
+        value={fieldValues.phone}
+        onChange={changeHandler}
+        required
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        value={fieldValues.email}
+        onChange={changeHandler}
+        required
+      />
+
+      <button>Continue</button>
+    </form>
+  );
+}
+
+export default ContactForm;
