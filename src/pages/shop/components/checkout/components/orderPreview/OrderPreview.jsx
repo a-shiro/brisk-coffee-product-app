c56b498d1fd616bd52dfc1408f8abe428dbb062b
a-shiro <alekmitov20@gmail.com>
2023-10-23 10:40:42 +0300

    Add: Cart preview

--- src/pages/shop/components/checkout/components/orderPreview/OrderPreview.jsx
+++ src/pages/shop/components/checkout/components/orderPreview/OrderPreview.jsx
@@ -1,13 +1,38 @@
+// Hooks
+import { useContext } from "react";
+// Context
+import { CartContext } from "../../../../context/context";
+// CSS
+import styles from "./OrderPreview.module.css";
+
 function OrderPreview() {
+  const { cart } = useContext(CartContext);
+
   return (
-    <div>
-      <header>
-        <h3>My Order (1)</h3>
+    <div className={styles.cart}>
+      <header className={styles.header}>
+        <h3>My Order ({cart.quantity})</h3>
       </header>
 
-      {/* 
-      <ProductList />
-      <CheckoutBtn /> */}
+      <ul className={styles.items}>
+        {Object.values(cart.items).map(({ product, quantity }) => {
+          return (
+            <li key={product.id}>
+              <p>
+                {product.brand} - {product.flavour} (x{quantity})
+              </p>
+              <div>
+                <span>${(quantity * product.price).toFixed(2)}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className={styles.costContainer}>
+        <p>Total</p>
+        <p>${cart.subtotal.toFixed(2)}</p>
+      </div>
     </div>
   );
 }
