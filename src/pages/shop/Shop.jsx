7628577a9fb6dae8920a4957915ee82adab28e3d
a-shiro <alekmitov20@gmail.com>
2023-10-23 11:50:50 +0300

    ---

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 // Components
 import Checkout from "./components/checkout/Checkout";
 import Menu from "./components/menu/Menu";
-// CSS
-import styles from "./Shop.module.css";
 // Context
 import { CheckoutContext, CartContext } from "./context/context";
 
@@ -13,8 +11,6 @@ function Store() {
 
   const [cart, setCart] = useState({ quantity: 0, subtotal: 0, items: {} });
   const [cartMenuActive, setCartMenuActive] = useState(false);
-
-  // ToDo: Transfer cart + context here
 
   return (
     <CheckoutContext.Provider value={{ checkoutActive, setCheckoutActive }}>
