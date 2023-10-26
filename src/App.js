a492e97693aee582d9f591c5ee80bce393c08e67
a-shiro <alekmitov20@gmail.com>
2023-10-26 11:38:21 +0300

    Delete: App.css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/common/nav/Nav";
 import RoutesManager from "./routes/RoutesManager";
 import Footer from "./components/common/footer/Footer";
-// CSS
-import "./App.css";
 
 function App() {
   return (
